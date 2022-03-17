85e2aba3022b9220dd3ea410918fde5cee6c656b
yongchang he <hyc0812@163.com>
2022-03-17 15:51:56 -0600

    version 1.1

--- interact_with_kaleido.py
+++ interact_with_kaleido.py
@@ -1,3 +1,4 @@
+from textwrap import indent
 import requests
 import json
 from random import randrange
@@ -5,8 +6,8 @@ node1Header = {"Content-Type": "application/json",
                "accept": "application/json",
                "Authorization": "Basic dTB3ODR1Y2Z4czpJbVhHUElWOEVyV2Z4ZEZxS2toWHM5aTZRb3ZGb0tobUpQN1YxVTZyZHZZ",
                # Signing address deployed the contract
-               "kld-from": "0x0A14954680Ed13ae3Ddf2B2f31Be1721Ae5A53f3",
-               "kld-sync": "true",
+               "x-kaleido-from": "0x0A14954680Ed13ae3Ddf2B2f31Be1721Ae5A53f3",
+               "x-kaleido-sync": "true",
               }
 
 node2Header = {"Content-Type": "application/json",
@@ -28,39 +29,41 @@ appURI  = baseURI + address
 
 getRouteURI = baseURI + "/get"
 postRouteURI = baseURI + "/set"
-#
-#tx = {"x": "29"}
 
 # Get request api call
-#response = requests.get(getRouteURI, headers=node1Header)
-#print(response.status_code);
+# response = requests.get(getRouteURI, headers=node1Header)
+# print(response.status_code)
+
+# if (response.status_code == 200 ):
+#   print(response.json())
+
+tx={"x": str(randrange(99)) }
+response = requests.post(postRouteURI, json=tx, headers=node1Header)
+print(response.status_code)
+
+if (response.status_code == 200 or response.status_code == 201):
+  print (json.dumps(response.json(), indent=4, sort_keys=True))
+
 
 #if (response.status_code == 200 ):
 #  print(response.json())
+# Here we run a post request 100 times with a random transaction number
+# Then print the time it took to commit the transaction
+# We'll post the transaction to node1 and read the transaction from node2
 
-# Here we run a post request 100 times with a random transaction number
-# Then print the time it took to commit the transaction
-# We'll post the transaction to node1 and read the transaction from node2
-for x in range(100):
-  
-  tx={"x": str(randrange(99)) }
-
-  # Send a post request
-  response = requests.post(postRouteURI, json=tx, headers=node1Header)
- 
-  # Validate the response code
-  if (response.status_code == 200 or response.status_code == 201):
-    
-    # Uncomment this if you would like to see the transaction response
-    #print (json.dumps(response.json(), indent=4, sort_keys=True))
-    
-    # Print the time it took from the elapse time from the response header
-    print (response.json()['headers']['timeElapsed'])
-  
-  
-  # If you would like to see the response of the get request, 
-  # uncomment the below section of the code.
-  response = requests.get(getRouteURI, headers=node2Header)
-  if (response.status_code == 200 ):
-    #print(response.json())
-    print(json.dumps(response.json(), indent=4, sort_keys=True))
+# for x in range(100):
+#   tx={"x": str(randrange(99)) }
+#   # Send a post request
+#   response = requests.post(postRouteURI, json=tx, headers=node1Header)
+#   # Validate the response code
+#   if (response.status_code == 200 or response.status_code == 201):
+#     # Uncomment this if you would like to see the transaction response
+#     #print (json.dumps(response.json(), indent=4, sort_keys=True))
+#     # Print the time it took from the elapse time from the response header
+#     print (response.json()['headers']['timeElapsed'])
+#   # If you would like to see the response of the get request, 
+#   # uncomment the below section of the code.
+#   response = requests.get(getRouteURI, headers=node2Header)
+#   if (response.status_code == 200 ):
+#     #print(response.json())
+#     print(json.dumps(response.json(), indent=4, sort_keys=True))
